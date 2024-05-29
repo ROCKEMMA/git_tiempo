78755bf640d37fa5d53758dc111294504a729713
jefuentes <jossuerm55@gmail.com>
2024-05-29 11:40:59 -0600

    Actualiazación del header modo rockemma

---
+++ header/header.js
@@ -0,0 +1,8 @@
+let header_DOM = document.querySelector('.header');
+
+header_DOM.innerHTML = `
+
+    <h1 class="titulo">Comandos <span>GIT</span></h1>
+    <a href="https://github.com/ROCKEMMA/git_tiempo" class="btn-github">github</a>
+
+`;
